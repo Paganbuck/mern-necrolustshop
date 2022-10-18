922a74828cee02f9724791977b6fea2577e65f85
Juan Diego Bedoya <jdbedoya928@gmail.com>
2022-10-17 21:57:10 -0500

    NecroLust-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Necrolust Records</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
