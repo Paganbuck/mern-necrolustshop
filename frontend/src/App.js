7c2e4266f8c804728075debf7639bf3f8d7fa205
Juan Diego Bedoya <jdbedoya928@gmail.com>
2022-10-23 23:32:51 -0500

    Ingresamos más productos y modificamos el css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <Link to="/"><h1>Esmeralda</h1></Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen/>} />
-          < Route path="/" element={<HomeScreen/>} />
-        </Routes>
-        
-      </main>
+    <div className="grid-container">
+        <header className="row">
+            <div>
+              <a className="brand" href="/">ESMERALDA </a></div>
+            <div>
+                <a href="/cart">Productos</a>
+                <a href="/signin">Carrito</a>
+            </div>
+        </header>
+        <main>
+            <div>
+            <div className="row center">
+              {
+                data.products.map(product =>(
+                  <div key={product._id} className="card">
+                    <a href={`/product/${product._id}`}>                        
+                    <img className="medium" src={product.image} alt={product.name}></img>
+                    </a>
+                    <div className="card-body">
+                    <a href={`/product/${product._id}`}> 
+                          <h2>{product.name}</h2>
+                            
+                        </a>
+                        <div className="rating">
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>
+                            <span><i className="fa fa-star"></i></span>                            
+                            <div className="price">${product.price}</div>
+                        </div>
+                    </div>
+                </div>
+                ))
+              }
+                
+                
+            </div>
+            </div>
+        </main>
+        <footer className="row center">
+            All right reserve
+        </footer>
     </div>
-    </BrowserRouter>
   );
 }
+  
 
 export default App;
